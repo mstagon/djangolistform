ff6cfebea405048ab46a43346c3c86a08d268b97
lsw02 <sw020726@naver.com>
2023-05-26 13:06:13 +0900

    Initial commit

---
+++ first/admin.py
@@ -0,0 +1,14 @@
+from django.contrib import admin
+from .models import Question
+from .models import Answer
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+admin.site.register(Question, QuestionAdmin)
+admin.site.register(Answer, AnswerAdmin)
